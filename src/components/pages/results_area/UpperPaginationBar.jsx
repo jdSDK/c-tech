3cd5efe59f42edf8ac98819bc327333168dc792b
José Simões <jdias.pro.office@gmail.com>
2025-04-05 15:48:46 +0100

    Improvements on upper pagination

--- src/components/pages/results_area/UpperPaginationBar.jsx
+++ src/components/pages/results_area/UpperPaginationBar.jsx
@@ -1,16 +1,49 @@
 import * as Styled from "./UpperPaginationBar.styled";
+import { useState, useRef } from "react";
 
-const UpperPaginationBar = ({ metadata, resultsPerPage, setResultsPerPage }) => {
+const UpperPaginationBar = ({
+    metadata,
+    resultsPerPage,
+    setResultsPerPage
+}) => {
+    /*
+    Display the results per page and the range/total number of results for the current query params.
+    Uses Ref to store the input value for the CountChanger to prevent losing focus on the input
+    field when state is updated.
+    The CountChanger works with both blur and enter key press to update the results per page.
+    The input field is validated to ensure that the value is a positive integer.
+    */
+    const valueRef = useRef(resultsPerPage);
+    const [isValid, setIsValid] = useState(true);
+
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        if (!isNaN(value) && value > 0) {
+            valueRef.current = value;
+            setIsValid(true);
+        } else {
+            setIsValid(false);
+        }
+    };
+
+    const handleValueUpdate = () => {
+        if (isValid && valueRef.current > 0) {
+            setResultsPerPage(parseInt(valueRef.current, 10));
+        } else {
+            valueRef.current = resultsPerPage;
+            setIsValid(true);
+        }
+    };
+
     const CountChanger = () => {
         return (
             <Styled.PerPageInput
                 name="update-results-per-page"
                 defaultValue={resultsPerPage}
-                onKeyUp={(e) => {
-                    if (e.key === "Enter") {
-                        setResultsPerPage(parseInt(e.target.value));
-                    }
-                }}
+                isValid={isValid}
+                onChange={handleInputChange}
+                onKeyUp={(e) => { if (e.key === "Enter") handleValueUpdate() }}
+                onBlur={handleValueUpdate}
             />
         )
     }
@@ -43,7 +76,8 @@ const UpperPaginationBar = ({ metadata, resultsPerPage, setResultsPerPage }) => 
                             </Styled.Count>
                             results
                             {
-                                metadata.filteredCount != metadata.totalCount && " for applied filters"
+                                metadata.filteredCount != metadata.totalCount
+                                && " for applied filters"
                             }
                         </>
                         :
